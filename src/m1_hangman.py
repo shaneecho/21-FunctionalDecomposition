b37a16eaeb1201153f104a96a66ab5e6bf605794
yans2 <yans2@rose-hulman.edu>
2019-01-28 20:22:10 -0500

    DONE

--- src/m1_hangman.py
+++ src/m1_hangman.py
@@ -9,21 +9,19 @@ Authors: Shixin Yan and Weizhou Liu.
 import random
 
 def main():
-    check_letter()
-
-def setting_up_letter():
     print('I will choose a random secret word from a dictionary. You set the MINIMUM length of that word.')
     MINIMUM_length = int(input('What MINIMUM length do you want for the secret word? '))
     print('It is your time to set the difficulty you want this game to have, with range from 0 to 10')
     HP = int(input('How many successful choioces do you want to allow yourself?'))
-    return MINIMUM_length, HP
+    get_a_random_letter_from_a_text_file(MINIMUM_length)
+    letter = input('What letter do you want to try?')
+    check_letter(letter,HP,MINIMUM_length)
 
-def get_a_random_letter_from_a_text_file():
+def get_a_random_letter_from_a_text_file(MINIMUM_length):
     with open('words.txt') as f:
         f.readline()
         string = f.read()
     word = string.split()
-    MINIMUM_length,HP= setting_up_letter()
     while True:
         r = random.randrange(0, len(word))
         item = word[r]
@@ -32,25 +30,12 @@ def get_a_random_letter_from_a_text_file():
     ret = ''
     for k in range(len(item)):
         ret = ret + '- '
-    print('Here is what you currently know about the secret word:')
     print(ret)
-    return item
+    return item,ret
 
-def receive_letter_from_user():
-    letter=input('What letter do you want to try?')
-    return letter
-
-def check_letter():
-    item=get_a_random_letter_from_a_text_file()
-    letter=receive_letter_from_user()
-    for k in range(len(item)):
-        if item[k]==letter:
-
-
-
-
-
-
+def check_letter(letter,HP,MINIMUM_length):
+    item,ret = get_a_random_letter_from_a_text_file(MINIMUM_length)
+    for k in range()
 
 
 
